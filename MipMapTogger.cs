637c299fb2440155a44e0a37d399f5ed58a99a2b
wanliyun <243258341@qq.com>
2017-07-23 22:38:06 +0800

    texture mipmap turn on/off in batch

---
+++ MipMapTogger.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+public class test : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    [MenuItem("texture/disable mipmaps")]
+    public static void Test1()
+    {
+        Debug.Log("*** FINDING ASSETS BY TYPE ***");
+
+        string[] guids;
+
+        // search for a ScriptObject called ScriptObj
+        guids = AssetDatabase.FindAssets("t:texture2D", new string[] {"Assets/Standard Assets"});
+        foreach (string guid in guids)
+        {
+            string setingFilePath = AssetDatabase.GUIDToAssetPath(guid) ;
+            TextureImporter setting = TextureImporter.GetAtPath(setingFilePath) as TextureImporter;
+            if (setting && setting.textureType == TextureImporterType.Advanced &&
+                setting.mipmapEnabled)
+            {
+                setting.mipmapEnabled = false;
+                
+                Debug.Log("texture2D: " + setting.mipmapEnabled + " type=" + setting.textureType + " path=" + setingFilePath);
+                setting.SaveAndReimport();
+            }
+            else
+            {
+                //Debug.Log("texture2D:  path=" + setingFilePath);
+            }
+        }
+    }
+
+    [MenuItem("texture/enable mipmaps")]
+    public static void Test2()
+    {
+        Debug.Log("*** FINDING ASSETS BY TYPE ***" + Application.persistentDataPath);
+    }
+}
